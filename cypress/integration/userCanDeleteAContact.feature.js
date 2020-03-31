a0f7f4847ce5072fec504825516898fb4ba013fc
Steve <designerofthings@gmail.com>
2020-03-31 16:01:16 +0200

    reinstalling yarn, adding browser-sync, custom script, css and styling

---
+++ cypress/integration/userCanDeleteAContact.feature.js
@@ -0,0 +1,10 @@
+describe ('user can delete a contact', () => {
+    before(() => {
+        cy.visit('http://localhost:3001')
+    })
+
+    it('by clicking the "Delete Contact" button', () => {
+        
+        })
+
+})
